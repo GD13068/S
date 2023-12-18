0f38659a9b16a5e2c15406fc898f1774bd6948c3
GD13068 <154277348+GD13068@users.noreply.github.com>
2023-12-18 15:25:18 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,128 @@
+const data = [
+    {
+        date: "December 21, 2023",
+        list: [
+            {
+                showName: "All Creatures Great and Small",
+                episodeName: "Christmas Special 2023",
+                seasonNumber: "4",
+                episodeNumberInSeason: "7",
+                airTime: "3:00",
+                channel: "Channel 5"
+            },
+        ]
+    },
+    {
+        date: "December 22, 2023",
+        list: []
+    },
+    {
+        date: "December 23, 2023",
+        list: [
+            {
+                showName: "Night Court",
+                episodeName: "A Night Court Before Christmas",
+                seasonNumber: "2",
+                episodeNumberInSeason: "1",
+                airTime: "7:35",
+                channel: "NBC"
+            },
+        ]
+    },
+    {
+        date: "December 24, 2023",
+        list: []
+    },
+    {
+        date: "December 25, 2023",
+        list: [
+            {
+                showName: "Call the Midwife",
+                episodeName: "Holiday Special 2023",
+                seasonNumber: "13",
+                episodeNumberInSeason: "0",
+                airTime: "7:00",
+                channel: "PBS"
+            },
+        ]
+    },
+    {
+        date: "December 26, 2023",
+        list: []
+    },
+    {
+        date: "December 27, 2023",
+        list: []
+    },
+    {
+        date: "December 28, 2023",
+        list: []
+    },
+    {
+        date: "December 29, 2023",
+        list: []
+    },
+    {
+        date: "December 30, 2023",
+        list: []
+    },
+    {
+        date: "December 31, 2023",
+        list: []
+    },
+    {
+        date: "January 1, 2024",
+        list: [
+            {
+                showName: "America's Got Talent: Fantasy League",
+                episodeName: "TBA",
+                seasonNumber: "1",
+                episodeNumberInSeason: "1",
+                airTime: "7:00",
+                channel: "NBC"
+            },
+        ]
+    },
+    {
+        date: "January 2, 2024",
+        list: [
+            {
+                showName: "Night Court",
+                episodeName: "TBA",
+                seasonNumber: "2",
+                episodeNumberInSeason: "2",
+                airTime: "7:00",
+                channel: "NBC"
+            },
+        ]
+    },
+    {
+        date: "January 3, 2024",
+        list: []
+    },
+    {
+        date: "January 4, 2024",
+        list: []
+    },
+    {
+        date: "January 5, 2024",
+        list: []
+    },
+    {
+        date: "January 6, 2024",
+        list: []
+    },
+    {
+        date: "January 7, 2024",
+        list: [
+            {
+                showName: "Call the Midwife",
+                episodeName: "Episode 1",
+                seasonNumber: "13",
+                episodeNumberInSeason: "1",
+                airTime: "2:00",
+                channel: "BBC One"
+            },
+        ]
+    },
+];
